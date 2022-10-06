1bdf6ac731ebf0631a52a00f2335e0a316f50cb7
Leticia <letyakron2@gmail.com>
2022-10-05 21:26:31 -0500

    Se realizo tarjeta con detalles

---
+++ public/js/details.js
@@ -0,0 +1,65 @@
+const getPokemonData =  async (id) => {
+	const pokemon =  await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+	const {data} = pokemon;
+    console.log (data);
+    document.getElementById("lblName").innerText = data.name;
+    document.getElementById("img").src = data.sprites.other['official-artwork'].front_default;
+    let abilities = data.abilities.map(ability => '<span class="badge text-bg-primary">'+ability.ability.name+'</span>').join(" ");  
+    document.getElementById("abilities").innerHTML = abilities;
+    document.getElementById("lblWeight").innerText = data.weight+" lb";
+   
+    drawSprites(data.sprites.back_default);
+    if (data.sprites.back_default_female){
+        drawSprites(data.sprites.back_default_female);
+    }
+
+    drawSprites(data.sprites.back_shiny);
+    if (data.sprites.back_shiny_female){
+        drawSprites(data.sprites.back_shiny_female);
+    }
+
+    drawSprites(data.sprites.front_default);
+    if (data.sprites.front_default_female){
+        drawSprites(data.sprites.front_default_female);
+    }
+    
+    drawSprites(data.sprites.front_shiny);
+    if (data.sprites.front_shiny_female){
+        drawSprites(data.sprites.front_shiny_female);
+    }
+        
+}
+
+const drawSprites = (url) =>{
+    let dgSprites =document.getElementById("sprites");
+    dgSprites.innerHTML += `
+        <div class="col-md-3 col-lg-2">
+            <img src="${url}" class="w100" alt="">
+        </div>
+        
+    `;
+
+
+}
+
+document.addEventListener( 'DOMContentLoaded', function() {
+    let queryString = window.location.search;
+    queryString = queryString.substring(1);
+    let params = null;
+    queryString.split("&").forEach(p => {
+        const temp = p.split("=");
+        if(temp.length == 2) {
+            if(!params) {
+                params = {};
+            }
+            params[temp[0]] = temp[1];
+        }
+    });
+    if (params && params.pokemon) {
+        getPokemonData (params.pokemon);
+    }
+} );
+
+
+
+
